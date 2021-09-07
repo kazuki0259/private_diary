96f92b2e768ad033217898c357e7ff83bb382949
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2021-09-07 12:33:30 +0900

    お問い合わせフォームの作成

--- diary/views.py
+++ diary/views.py
@@ -2,6 +2,8 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+from .forms import InquiryForm
+
 # Create your views here.
 
 def index(request):
@@ -9,3 +11,8 @@ def index(request):
 
 class IndexView(generic.TemplateView):
     template_name="diary/index.html"
+
+class InquiryView(generic.FormView):
+    template_name="diary/inquiry.html"
+    form_class = InquiryForm
+
