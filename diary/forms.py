183c74f0cd20668558b4e0723bbf9f511220797b
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2021-09-09 08:35:31 +0000

    テンプレートの改変

--- diary/forms.py
+++ diary/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.mail import EmailMessage
 
-
 class InquiryForm(forms.Form):
     name=forms.CharField(label='お名前',max_length=30)
     email=forms.EmailField(label='メールアドレス')
