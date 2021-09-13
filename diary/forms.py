b895f55f591ab79557593bee0772ec0dbbf9eabc
kazuki0259 <ksw2070302@stu.o-hara.ac.jp>
2021-09-13 12:38:04 +0900

    create 作成完了

--- diary/forms.py
+++ diary/forms.py
@@ -40,3 +40,14 @@ class InquiryForm(forms.Form):
 
         msg = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
         msg.send()
+
+
+from .models import Diary
+class DiaryCreateForm(forms.ModelForm):
+    class Meta:
+        model = Diary
+        fields = ('title', 'content', 'photo1', 'photo2', 'photo3',)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            for field in self.fields.values():
+                field.widget.attrs['class'] = 'form-control'
